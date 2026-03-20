a7e11ba8495ca69ed5e261e92cf42eaf4269ab44
rayleiighh <r.benlhaj@students.ephec.be>
2026-03-20 14:07:11 +0100

    chore(pwa): installation et configuration de vite-plugin-pwa #32

--- gaprc-frontend/vite.config.ts
+++ gaprc-frontend/vite.config.ts
@@ -1,11 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // Et celle-ci aussi 
+    tailwindcss(), 
+    // CONFIGURATION DE LA PWA (Issue 7)
+    VitePWA({
+      registerType: 'autoUpdate', // Met à jour l'app automatiquement chez les clients
+      includeAssets: ['favicon.svg', 'icons.svg', 'apple-touch-icon.png', 'masked-icon.svg'], // On inclut tes SVG existants
+      manifest: {
+        name: 'Sports Center POS',
+        short_name: 'SportsCenter',
+        description: 'Application de gestion de caisse et dashboard du Hall Omnisport',
+        theme_color: '#dc2626', // Le rouge de ton app
+        background_color: '#F8F9FA',
+        display: 'standalone', // Cache la barre d'URL pour faire comme une vraie app
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
 })
